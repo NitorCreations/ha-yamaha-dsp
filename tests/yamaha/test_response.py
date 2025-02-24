2b835869a16e288568c946c543831334743d01f3
Sam Stenvall <sam.stenvall@nitor.com>
2025-02-24 11:41:55 +0200

    Add tests for OKm responses

--- tests/yamaha/test_response.py
+++ tests/yamaha/test_response.py
@@ -5,10 +5,15 @@ from custom_components.yamaha_dsp.yamaha.response import ErrorResponse, OkRespon
 
 class YamahaResponseTest(unittest.TestCase):
     def test_parse_response(self):
-        ok_response = parse_response('OK devstatus runmode "normal"\n')
+        ok_response = parse_response('OK devstatus runmode "normal"' + "\n")
         self.assertIsInstance(ok_response, OkResponse)
         self.assertEqual(["OK", "devstatus", "runmode", "normal"], ok_response.parsed_response)
         self.assertEqual("normal", ok_response.value)
+
+        okm_response = parse_response('OKm set MTX:Index_33 0 0 1 "1"' + "\n")
+        self.assertIsInstance(okm_response, OkResponse)
+        self.assertEqual(["OKm", "set", "MTX:Index_33", "0", "0", "1", "1"], okm_response.parsed_response)
+        self.assertEqual("1", okm_response.value)
 
         complex_ok_response = parse_response('OK ssinfo 0 "" reserve "" ""' + "\n")
         self.assertIsInstance(ok_response, OkResponse)

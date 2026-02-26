b11707f134dfef7d99051860f8c687f737f9efd3
Pasi Niemi <pasi.niemi@nitor.com>
2026-02-26 19:04:50 +0200

    Address comments in PR #14

--- custom_components/yamaha_dsp/select.py
+++ custom_components/yamaha_dsp/select.py
@@ -1,5 +1,6 @@
 import logging
 
+from bidict import bidict
 from homeassistant.components.select import SelectEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceInfo
@@ -35,8 +36,7 @@ class RouterSelectEntity(SelectEntity):
         self._device_info = device_info
 
         self._source_param = create_index_parameter(self._config.index_source)
-        self._label_to_value = {source.label: source.value for source in self._config.sources}
-        self._value_to_label = {source.value: source.label for source in self._config.sources}
+        self._source_bidict = bidict({source.value: source.label for source in self._config.sources})
         self._current_option: str | None = None
 
     @property
@@ -57,7 +57,7 @@ class RouterSelectEntity(SelectEntity):
 
     @property
     def options(self) -> list[str]:
-        return [source.label for source in self._config.sources]
+        return list(self._source_bidict.values())
 
     @property
     def current_option(self) -> str | None:
@@ -66,13 +66,13 @@ class RouterSelectEntity(SelectEntity):
     async def async_update(self) -> None:
         source_response = await self._device.query_parameter_raw(self._source_param)
         source_value = source_response.get_int_value()
-        self._current_option = self._value_to_label.get(source_value)
+        self._current_option = self._source_bidict.get(source_value)
 
         if self._current_option is None:
             logger.warning("Router %s returned unknown source value %s", self._config.name, source_value)
 
     async def async_select_option(self, option: str) -> None:
-        source_value = self._label_to_value.get(option)
+        source_value = self._source_bidict.inverse.get(option)
         if source_value is None:
             raise ValueError(f"Invalid option '{option}' for router '{self._config.name}'")
 
